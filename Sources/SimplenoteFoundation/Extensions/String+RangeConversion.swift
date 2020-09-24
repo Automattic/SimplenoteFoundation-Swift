03acdd27ddbe6887880f9768fce4c9f4555cb9db
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-24 18:45:50 -0300

    New String Location API

--- Sources/SimplenoteFoundation/Extensions/String+RangeConversion.swift
+++ Sources/SimplenoteFoundation/Extensions/String+RangeConversion.swift
@@ -5,17 +5,28 @@ import Foundation
 //
 extension String {
 
+    /// Converts a Location (expressed as Integer) into a String.Index
+    ///
+    public func indexFromLocation(_ location: Int) -> String.Index? {
+        guard let unicodeLocation = utf16.index(utf16.startIndex, offsetBy: location, limitedBy: utf16.endIndex),
+            let location = unicodeLocation.samePosition(in: self) else {
+                return nil
+        }
+
+        return location
+    }
+
+    /// Maps a `String.Index` in the receiver's coordinates into coordinates valid for the specified Substring, in the target Range
+    ///
+    public func transportIndex(_ index: String.Index, to substring: String, in range: Range<String.Index>) -> String.Index? {
+        let locationInSubstring = distance(from: startIndex, to: index) - distance(from: startIndex, to: range.lowerBound)
+        return substring.index(substring.startIndex, offsetBy: locationInSubstring)
+    }
+
     /// Converts a `Range<String.Index>` into an UTF16 NSRange.
     ///
-    /// - Parameters:
-    ///     - range: the range to convert.
-    ///
-    /// - Returns: the requested `NSRange`.
-    ///
     public func utf16NSRange(from range: Range<String.Index>) -> NSRange {
-        guard let lowerBound = range.lowerBound.samePosition(in: utf16),
-            let upperBound = range.upperBound.samePosition(in: utf16) else
-        {
+        guard let lowerBound = range.lowerBound.samePosition(in: utf16), let upperBound = range.upperBound.samePosition(in: utf16) else {
             fatalError()
         }
 
--- Tests/SimplenoteFoundationTests/Extensions/StringRangeConversionTests.swift
+++ Tests/SimplenoteFoundationTests/Extensions/StringRangeConversionTests.swift
@@ -6,6 +6,57 @@ import XCTest
 //
 class StringRangeConversionTests: XCTestCase {
 
+    /// Verifies that `indexFromLocation` converts an UTF16 Location into a valid String.Index
+    ///
+    func testIndexFromLocationProperlyConvertsUTF16RangeIntoStringIndex() {
+        let samples: [(NSString, String, String)] = [
+            ("Hello World!", "World", "Hello "),
+            ("Hello 🌍!", "🌍", "Hello "),
+            ("Hello 🇮🇳!", "🇮🇳", "Hello "),
+            ("Hello 🇮🇳 🌍!", "🌍", "Hello 🇮🇳 ")
+        ]
+
+        for (nsString, wordToCapture, expectedPrefix) in samples {
+            let string: String = nsString as String
+            let nsRange = nsString.range(of: wordToCapture)
+            let index = string.indexFromLocation(nsRange.location)!
+
+            let wordCaptured = String(string.prefix(upTo: index))
+
+            XCTAssertEqual(expectedPrefix, wordCaptured)
+        }
+    }
+
+    /// Verifies that `relativeLocation(for: in:)` does not alter the Location, whenever the specified Range covers the full string
+    ///
+    func testTransportIndexReturnsTheUnmodifiedIndexWhenTheRangeEnclosesTheFullReceiverRange() {
+        let text = "this 🌎 is supposed to be a single but relatively long line of text"
+        let fullRange = text.startIndex ..< text.endIndex
+
+        for location in Int.zero ..<  text.count {
+            let index = text.index(text.startIndex, offsetBy: location)
+            XCTAssertEqual(text.transportIndex(index, to: text, in: fullRange), index)
+        }
+    }
+
+    /// Verifies that `transportIndex(_:to:in:)` converts the specified Absolute Location into a Relative Location, with regards of a specified range
+    ///
+    func testTransportIndexReturnsTheExpectedLocationWhenTheRangeIsNotTheFullString() {
+        let lhs = "this 🌎 "
+        let substring = "is supposed to be"
+        let rhs = " a single but relatively long line of text"
+
+        let text = lhs + substring + rhs
+        let substringRange = text.range(of: substring)!
+
+        for location in Int.zero ..< substring.count {
+            let unmappedIndex = text.index(text.startIndex, offsetBy: location + lhs.count)
+            let expectedIndex = substring.index(substring.startIndex, offsetBy: location)
+
+            XCTAssertEqual(text.transportIndex(unmappedIndex, to: substring, in: substringRange), expectedIndex)
+        }
+    }
+
     /// Verifies that `utf16NSRange` properly converts a `Range<String.Index>` into a NSString NSRange
     ///
     func testUTF16NSRangeProperlyConvertsSwiftRangesIntoNSStringRanges() {
